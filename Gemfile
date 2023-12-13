d09850349485be3b6534529af50b5f42b058d348
Sakamoto505 <93153773+Sakamoto505@users.noreply.github.com>
2023-12-13 17:07:05 +0300

    Add turbo-frame to search for words without rebooting

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'pagy'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.8'
+gem 'turbo-rails'
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem 'sprockets-rails'
--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -8,7 +8,12 @@ class WordsController < ApplicationController
   def search
     pagy, words_results = pagy(WordsSearch.call(**search_params))
 
-    render 'search', locals: { pagy: pagy, words_results: words_results }
+    respond_to do |format|
+      format.html { render 'search', locals: { pagy: pagy, words_results: words_results } }
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace('words_results', partial: 'shared/words_results', locals: { pagy: pagy, words_results: words_results })
+      end
+    end
   end
 
   def show
